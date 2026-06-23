d2b645a1af8634ec3a51de685130857ca92dec19
Vaibhava Kumar Shrivastav <vaibhavakumarshrivastav@gmail.com>
2026-06-23 16:02:04 +0530

    add file news task

--- weather task/script.js
+++ weather task/script.js
@@ -1,7 +1,9 @@
 const btn=document.getElementById("btn")
 const output=document.getElementById("weather");
 const loading=document.getElementById("loading");
+const news=document.getElementById("news");
 const API_KEY = "d0551800686e58bc5c16cd13f8cb06e2";
+const nAPI="7b937d195e9dee305e71d922e264cd01"
 
 btn.addEventListener("click",async (e)=>{
     const city=document.getElementById("input").value.trim();
@@ -25,11 +27,40 @@ btn.addEventListener("click",async (e)=>{
         }
         output.innerHTML = `
             <h2>${data.location.name}</h2>
-            <p>🌡 Temperature: ${data.current.temperature}°C</p>
-            <p>☁ Weather: ${data.current.weather_descriptions[0]}</p>
-            <p>💧 Humidity: ${data.current.humidity}%</p>
-            <p>🌬 Wind Speed: ${data.current.wind_speed} km/h</p>
+            <p> Temperature: ${data.current.temperature}°C</p>
+            <p> Weather: ${data.current.weather_descriptions[0]}</p>
+            <p> Humidity: ${data.current.humidity}%</p>
+            <p> Wind Speed: ${data.current.wind_speed} km/h</p>
         `;
+
+        const newsResponse = await fetch(
+            `https://api.mediastack.com/v1/news?access_key=${nAPI}&keywords=${city}&languages=en`
+        );
+
+        const ndata= await newsResponse.json();
+        news.innerHTML = `
+            <h2 style="margin-top:20px;">
+                📰 Latest News
+            </h2>
+        `;
+        if (!ndata.data || ndata.data.length === 0) {
+            news.innerHTML += `
+            <p>No news found</p>
+            `;
+        }
+        else {
+            ndata.data.slice(0, 3).forEach(article => {
+                news.innerHTML += `
+                <a
+                class="news-card"
+                href="${article.url}"
+                target="_blank"
+                >
+                ${article.title}
+                </a>
+                `;
+            });
+        }
     }
     catch(e){
         output.innerHTML = `
